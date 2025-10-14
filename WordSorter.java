fdcd203153e11db56b92bf19d92a2cb3db9bab7f
Preston Havenstein <meadehavenstein@gmail.com>
2025-10-14 16:48:32 +0000

    still working

--- WordSorter.java
+++ WordSorter.java
@@ -55,6 +55,7 @@ public class WordSorter
 			}//end for each
 		}//end while
 		int choice = 0;
+		in = new Scanner(System.in);
 		while(choice != 6)
 		{
 			System.out.println("Hello, you have the following options:");
@@ -67,26 +68,81 @@ public class WordSorter
 			choice = in.nextInt();
 			if(choice == 1)
 			{
-
-			}
+				System.out.println("Please enter a letter:");
+				in.nextLine();
+				String letter = in.nextLine();
+				if(sorted[letter.compareTo("a")].size() != 0)
+				{
+					System.out.println(sorted[letter.compareTo("a")]);
+				}//end if
+				else
+				{
+					System.out.println("No words start with this letter.");
+				}
+			}//end if
 			if(choice == 2)
 			{
-
-			}
+				if(words.size() != 0)
+				{
+					System.out.println(words);
+				}
+				else
+				{
+					System.out.println("Empty list");
+				}
+			}//end if
 			if(choice == 3)
 			{
-
-			}
+				if(words.size() != 0)
+				{
+					System.out.println("There are " + words.size() + " unique words in the article");
+				}
+				else
+				{
+					System.out.println("Empty list");
+				}
+			}//end if
 			if(choice == 4)
 			{
-
-			}
+				System.out.println("Please enter the word you would like to find:");
+				String find = in.nextLine();
+				boolean found = false;
+				for(int i=0; i<words.size(); i++)
+				{
+					if(words.get(i).equals(find))
+					{
+						System.out.println("Word found in article.");
+						found = true;
+					}//end else
+				}//end for
+				if(found = false)
+				{
+					System.out.println("Word NOT found in article.");
+				}//end if
+			}//end if
 			if(choice == 5)
 			{
-
-			}
+				System.out.println("Please enter the word you would like to remove:");
+				String delete = in.nextLine();
+				boolean found = false;
+				for(int i=0; i<words.size(); i++)
+				{
+					if(words.get(i).equals(delete))
+					{
+						words.remove(i);
+						found = true;
+					}//end if
+				}//end for
+				if(found == false)
+				{
+					System.out.println("Word NOT found in the article.");
+				}//end if
+				else
+				{
+					System.out.println("Word successfully removed from the list");
+				}//end else
+			}//end if
 		}//end while
-		
-		
+		in.close();
 	}//end main
 }//end class
